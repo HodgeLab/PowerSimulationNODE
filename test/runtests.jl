2d27a92958e11e59ef89c6eb17c1053e675d4844
Matt <67015312+m-bossart@users.noreply.github.com>
2022-01-11 11:40:51 -0700

    add test

---
+++ test/runtests.jl
@@ -0,0 +1,3 @@
+using Test
+
+include("test_hpc.jl")
---
+++ test/test_hpc.jl
@@ -0,0 +1,35 @@
+using Mustache
+using Logging
+include("../src/constants.jl")
+include("../src/HPCTrain.jl")
+
+struct TestParams
+    train_id::String
+end
+
+test = [TestParams("test"), TestParams("test2")]
+
+hpc_params = SavioHPCTrain(;
+    username = "test_user",
+    params_data = test,
+    project_folder = "test",
+    scratch_path = mktempdir(),
+    n_nodes = 2,
+)
+
+generate_train_files(hpc_params)
+file = read(hpc_params.train_bash_file, String)
+@test occursin("--slf hostfile", file)
+@test !occursin("SLURM_NPROCS", file)
+
+hpc_params = SavioHPCTrain(;
+    username = "test_user",
+    params_data = test,
+    project_folder = "test",
+    scratch_path = mktempdir(),
+)
+
+generate_train_files(hpc_params)
+file = read(hpc_params.train_bash_file, String)
+@test !occursin("--slf hostfile", file)
+@test occursin("SLURM_NPROCS", file)
