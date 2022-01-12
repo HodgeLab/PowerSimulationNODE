b372bcf092e1c41655cebea9a0c49b116d1dcdc0
Matt <67015312+m-bossart@users.noreply.github.com>
2022-01-11 18:03:44 -0700

    unused constants

--- src/constants.jl
+++ src/constants.jl
@@ -1,13 +1,3 @@
-#indices of specific variables in surrogate (for plotting)
-const I__IR_OUT = 22
-const I__II_OUT = 23
-const I__IR_FILTER = 5
-const I__II_FILTER = 19
-const I__IR_NN = 20
-const I__II_NN = 21
-
-#const ODE_ORDER = 19
-#const N_ALGEBRAIC_STATES = 2 
 
 const INPUT_FOLDER_NAME = "input_data"
 const OUTPUT_FOLDER_NAME = "output_data"
