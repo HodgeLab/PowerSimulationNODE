f6643cf190117628155cdda6c8e2eecd7ee2ca1a
m-bossart <bossart.matt@gmail.com>
2022-11-16 13:18:58 -0500

    only print id for data in overview

--- test/test_prettytable.jl
+++ test/test_prettytable.jl
@@ -2,10 +2,12 @@ path = (joinpath(pwd(), "test-pretty-print-dir"))
 !isdir(path) && mkdir(path)
 
 try
-    test1 = TrainParams(train_id = "test_1")
-    test2 = TrainParams(train_id = "test_2", lb_loss = 1.0)
+    test1 = TrainParams(train_id = "01")
+    test2 = TrainParams(train_id = "10", lb_loss = 1.0)
+    test3 = TrainParams(train_id = "02", lb_loss = 1.0)
     PowerSimulationNODE.serialize(test1, joinpath(path, "train_1.json"))
     PowerSimulationNODE.serialize(test2, joinpath(path, "train_2.json"))
+    PowerSimulationNODE.serialize(test3, joinpath(path, "train_3.json"))
 
     print_train_parameter_overview(path)
 
