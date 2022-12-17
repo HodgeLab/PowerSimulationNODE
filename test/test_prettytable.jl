94d753a145de21d93d526debc7058c974260f83c
Matt Bossart <bossart.matt@gmail.com>
2022-12-17 11:39:55 -0500

    add test sets

--- test/test_prettytable.jl
+++ test/test_prettytable.jl
@@ -1,17 +1,19 @@
-path = (joinpath(pwd(), "test-pretty-print-dir"))
-!isdir(path) && mkdir(path)
+@testset "Test serializing TrainParams and printing overview" begin
+    path = (joinpath(pwd(), "test-pretty-print-dir"))
+    !isdir(path) && mkdir(path)
 
-try
-    test1 = TrainParams(train_id = "01")
-    test2 = TrainParams(train_id = "10", rng_seed = 1)
-    test3 = TrainParams(train_id = "02", rng_seed = 2)
-    PowerSimulationNODE.serialize(test1, joinpath(path, "train_1.json"))
-    PowerSimulationNODE.serialize(test2, joinpath(path, "train_2.json"))
-    PowerSimulationNODE.serialize(test3, joinpath(path, "train_3.json"))
+    try
+        test1 = TrainParams(train_id = "01")
+        test2 = TrainParams(train_id = "10", rng_seed = 1)
+        test3 = TrainParams(train_id = "02", rng_seed = 2)
+        PowerSimulationNODE.serialize(test1, joinpath(path, "train_1.json"))
+        PowerSimulationNODE.serialize(test2, joinpath(path, "train_2.json"))
+        PowerSimulationNODE.serialize(test3, joinpath(path, "train_3.json"))
 
-    print_train_parameter_overview(path)
+        print_train_parameter_overview(path)
 
-finally
-    @info("removing test files")
-    rm(path, force = true, recursive = true)
+    finally
+        @info("removing test files")
+        rm(path, force = true, recursive = true)
+    end
 end
--- test/test_serialize.jl
+++ test/test_serialize.jl
@@ -1,14 +1,17 @@
-path = (joinpath(pwd(), "serialize-test-dir"))
-!isdir(path) && mkdir(path)
+@testset "Test serializing and reading back TrainParams" begin
+    path = (joinpath(pwd(), "serialize-test-dir"))
+    !isdir(path) && mkdir(path)
 
-try
-    test_params = TrainParams()
-    PowerSimulationNODE.serialize(test_params, joinpath(path, "test_params.jl"))
-    test_params_2 = TrainParams(joinpath(path, "test_params.jl"))
-    #Spot check
-    @test test_params.train_data.operating_points[1] ==
-          test_params_2.train_data.operating_points[1]
-finally
-    @info("removing test files")
-    rm(path, force = true, recursive = true)
+    try
+        test_params = TrainParams()
+        PowerSimulationNODE.serialize(test_params, joinpath(path, "test_params.jl"))
+        test_params_2 = TrainParams(joinpath(path, "test_params.jl"))
+        #Spot check
+        @test test_params.train_data.operating_points[1] ==
+              test_params_2.train_data.operating_points[1]
+        @test test_params.optimizer == test_params_2.optimizer
+    finally
+        @info("removing test files")
+        rm(path, force = true, recursive = true)
+    end
 end
